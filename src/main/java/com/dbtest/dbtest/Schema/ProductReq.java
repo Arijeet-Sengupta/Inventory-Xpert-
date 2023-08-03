f1edf6c9e391d1d042a8d4e50e6bb78a95dc6cb6
Isha Bali <ishabali6504@gmail.com>
2023-08-03 17:54:52 +0530

    ProductReq added

--- src/main/java/com/dbtest/dbtest/Schema/ProductReq.java
+++ src/main/java/com/dbtest/dbtest/Schema/ProductReq.java
@@ -7,22 +7,14 @@ public class ProductReq {
     @JsonProperty
     public Integer productId;
 
+
    public Integer getProductId() {
         return productId;
     }
-//
-//    public void setProductId(Integer productId) {
-//        this.productId = productId;
-//    }
+
 
     @JsonProperty
     public String productName;
 
-//    public String getProductname() {
-//        return productname;
-//    }
-//
-//    public void setProductname(String productname) {
-//        this.productname = productname;
-//    }
+
 }
