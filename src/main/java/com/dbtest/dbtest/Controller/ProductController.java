28ce713bd31dfad57d466e345e5ed9c3d61c4b56
Nayan Raut <nayanraut245@gmail.com>
2023-08-04 17:33:28 +0530

    Merging Updated Product controller

--- src/main/java/com/dbtest/dbtest/Controller/ProductController.java
+++ src/main/java/com/dbtest/dbtest/Controller/ProductController.java
@@ -6,6 +6,7 @@ import com.dbtest.dbtest.Schema.ProductReq;
 import com.dbtest.dbtest.Schema.ProductResponse;
 import com.dbtest.dbtest.Service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -21,7 +22,7 @@ public class ProductController {
 
     //adding details
     @PostMapping("/adddetails")
-    public ProductResponse createProduct (@RequestBody ProductReq productReq)
+    public ProductResponse createProduct (@Validated @RequestBody ProductReq productReq)
     {
 
         return productService.addorUpdate(productReq);
